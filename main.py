1480a41b5ed7bebfabe6f24e3efa5c176c53373b
Will <124865059+wwwillgunnn@users.noreply.github.com>
2024-02-13 08:40:45 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 """
-This is an eBay web scraper with the purpose of finding and storing ebay listings that have
-pests on the Exotic Environmental Pest List (EEPL)
+This is an eBay web scraper with the purpose of finding ebay listings that have things on the EEPL list
+Then that data is stored in a spreadsheet
 
-This module will only print out results to the terminal
+IMPORTANT NOTE: eBay has a limit on how many items you can search for in 1 session, 
+Do not run this module with the whole EEPL else you will run into this issue :^).
 
 Made by William Gunn
 """
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import time
 
-# EEPL
+# Exotic Environmental Pests List
 EEPL = [
     # Fresh water invertebrates
     "Asian clam", "Assassin snail", "Bloody-red mysid shrimp", "Chinese mitten crab", "Chinese mystery snail",
@@ -25,94 +28,82 @@ EEPL = [
     "New Zealand green-lipped mussel", "Red whelk", "Red-gilled mudworm", "Soft shelled clam", "Toxic dinoflagellate"
     # Terrestrial invertebrates
     "Africanised honeybee", "Annona mealybug", "Asian hornet", "Yellow-legged hornet", "Asian beetle mite", "Asian gypsy moth",
-    "Brown marmorated stink bug", "Cape honeybee", "Common eastern bumblebee", "Cycad aulacaspis scale", "Delta wasp", "Dichroplus grasshopper",
-    "Electric ant", "Formosan subterranean termite", "Giant african snail", "Gold dust weevil", "Harlequin lady beetle", "Multicolored Asian lady beetle",
-    "Honey bee tracheal mite", "Oriental powderpost beetle", "Picnic beetle", "Red imported fire ant", "Rosy predator snail", "Shot hole borer",
+    "Brown marmorated stink bug", "Cape honeybee", "Common eastern bumblebee", "Cycad aulacaspis scale", "Delta wasp",
+    "Dichroplus grasshopper",
+    "Electric ant", "Formosan subterranean termite", "Giant african snail", "Gold dust weevil", "Harlequin lady beetle",
+    "Multicolored Asian lady beetle",
+    "Honey bee tracheal mite", "Oriental powderpost beetle", "Picnic beetle", "Red imported fire ant",
+    "Rosy predator snail", "Shot hole borer",
     "Western drywood termite",
     # Vertebrate pests
-    "African pygmy hedgehog", "Asian black-spined toad", "Asian painted frog", "Boa constrictor", "Burmese python", "Chinese carp", "Climbing perch",
-    "Common snapping turtle", "Corn snake", "Fire bellied newt", "Flat-tailed house gecko", "Green iguana", "Grey squirrel", "House crow",
-    "Nile tilapia", "Oriental garden lizard", "Pacific rat", "Red-eared slider turtle", "Silver carp", "Snakeheads", "Stoat",
+    "African pygmy hedgehog", "Asian black-spined toad", "Asian painted frog", "Boa constrictor", "Burmese python",
+    "Chinese carp", "Climbing perch",
+    "Common snapping turtle", "Corn snake", "Fire bellied newt", "Flat-tailed house gecko", "Green iguana",
+    "Grey squirrel", "House crow",
+    "Nile tilapia", "Oriental garden lizard", "Pacific rat", "Red-eared slider turtle", "Silver carp", "Snakeheads",
+    "Stoat",
     "Veiled chameleon", "Walking catfish"
     # Weeds and freshwater alge
-    "Asiatic sans sedge", "Black sage", "Black swallow-wort", "Brittle naiad", "Cane tibouchina", "Didymo", "Halogeton", "Karoo thorn",
-    "Lagariosiphon", "Leafy spurge", "Limnocharis flava", "Manchurian wildrice", "Mikania", "Mouse-ear hawkweed", "Nepalese browntop", "Portuguese broom",
+                        "Asiatic sans sedge", "Black sage", "Black swallow-wort", "Brittle naiad", "Cane tibouchina",
+    "Didymo", "Halogeton", "Karoo thorn",
+    "Lagariosiphon", "Leafy spurge", "Limnocharis flava", "Manchurian wildrice", "Mikania", "Mouse-ear hawkweed",
+    "Nepalese browntop", "Portuguese broom",
     "Slangbos", "South african ragwort", "Spiked pepper", "Water primrose", "Wiregrass"
-    ]
+]
+# Create list for data frame
+data = []
 
 
-def scrape_page():
-    # Loop through all pages on site
+def scrape_pages():
+    with requests.Session() as session:
+        for eepl_species in EEPL:
+            get_data(session, eepl_species)
+    export_data(data)
+
+
+def get_data(session, eepl_species):
     page_number = 1
-    for EEPL_species in EEPL:
-        EEPL_species_listing = EEPL_species.replace(" ", "+")
-        # Get page information
-        page_url = f"https://www.ebay.com.au/sch/i.html?_from=R40&_nkw={EEPL_species_listing}&_sacat=190&rt=nc&_pgn={page_number}"
-        page = requests.get(page_url)
-        soup = BeautifulSoup(page.content, "html.parser")
-        # Get page result number
-        results = soup.find(id="mainContent")
-        navigation = results.find("nav", class_="pagination")
-        count = results.find(class_="srp-controls__count-heading")
-        count_number = count.find("span", class_="BOLD")
-        # Check page exists
-        if page_url is not None and int(count_number.text.strip().replace(',', '')) > 0:
-            # Get page data
-            get_data(soup)
-            # For when a page doesn't only have 1 page
-            if navigation is not None:
-                # Increment page number
-                int(page_number)
+    while True:
+        page_url = f"https://www.ebay.com.au/sch/i.html?_from=R40&_nkw={eepl_species.replace(' ', '+')}&_sacat=190&rt=nc&_pgn={page_number}"
+        response = session.get(page_url, headers={'User-Agent': 'Mozilla/5.0'})
+        soup = BeautifulSoup(response.text, "lxml")
+        # Get total item count of page --> maybe delete
+        count = soup.find(class_="srp-controls__count-heading")
+        count_number = count.find("span", class_="BOLD").get_text().strip()
+
+        if page_url is not None and int(count_number.replace(',', '')) > 0 and count_number is not None:
+            results = soup.find(id="srp-river-main")
+            product_listings = results.find_all("li", class_="s-item__pl-on-bottom")
+            # Use list comprehension for creating the data list
+            data.extend([
+                {"Title": item.find("span", role="heading").get_text().strip(),
+                 "Price": item.find("span", class_="s-item__price").get_text().strip(),
+                 "Seller Name": item.find("span", class_="s-item__seller-info-text").get_text().strip()
+                 if item.find("span", class_="s-item__seller-info") else "None",
+                 "Seller Location": item.find("span", class_="s-item__itemLocation").get_text().strip()
+                 if item.find("span", class_="s-item__itemLocation") else "Australia"}
+                for item in product_listings[1:]
+            ])
+            # Loop to the next page
+            navigation = results.find("nav", class_="pagination")
+            next_page = soup.find("a", class_="pagination__next")
+            if navigation is not None and next_page is not None:
                 page_number += 1
-                print(page_url)
+                time.sleep(1)  # Introduce a delay between requests
             else:
-                continue
+                break
         else:
-            continue
+            break
 
 
-def get_data(content):
-    # Get HTML data from listings
-    results = content.find(id="srp-river-main")
-    product_listings = results.find_all("li", class_="s-item__pl-on-bottom")
-    # Loop through each listing on the page (skip 1st listing)
-    for item in product_listings[1:]:
-        title = item.find("span", role="heading")
-        price = item.find("span", class_="s-item__price")
-        print(title.text.strip())
-        print(price.text.strip())
-        # if the format is different on the site
-        try:
-            seller_name = item.find("span", class_="s-item__seller-info")
-            print(seller_name.text.strip())
-        except AttributeError:
-            seller_name = item.find("span", class_="s-item__seller-info-text__field-0")
-            seller_name2 = item.find("span", class_="s-item__seller-info-text__field-1")
-            if (seller_name and seller_name2) is not None:
-                print(seller_name.text.strip())
-                print(seller_name.text.strip())
-            else:
-                seller_name = "None"
-                print(seller_name)
-        try:
-            seller_location = item.find("span", class_="s-item__location s-item__itemLocation")
-            print(seller_location.text.strip())
-        except AttributeError:
-            seller_location = "Australia"  # Maybe have 'None' I'm just assuming it's in Australia
-            print(seller_location)
-        try:
-            watching = item.find("span", class_="s-item__dynamic s-item__watchCountTotal")
-            print(watching.text.strip())
-        except AttributeError:
-            watching = "None watching"
-            print(watching)
-        print()
+def export_data(data_frame):
+    df = pd.DataFrame(data_frame)
+    df.to_excel("Results.xlsx")
 
 
 def main():
-    # Call scrape page function
-    print("Scraping page ...")
-    scrape_page()
+    print("Scraping pages...")
+    scrape_pages()
 
 
 if __name__ == "__main__":
